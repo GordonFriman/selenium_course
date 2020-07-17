c7b557fc368855ab3beeb83322ba839e55015dd5
Gordon Friman Work <gordon.f.v.v@ya.ru>
2020-07-17 14:34:22 +0300

    Изменения

--- section3/conftest.py
+++ section3/conftest.py
@@ -1,10 +1,24 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='opera',
+                     help="Choose browser: chrome or opera")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "opera":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Opera()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or opera")
     yield browser
     print("\nquit browser..")
     browser.quit()
